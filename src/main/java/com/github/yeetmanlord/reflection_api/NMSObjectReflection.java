2bda98f400af8ac42a2b2dc5307cef7c0e48718a
YeetmanLord <60905636+YeetmanLord@users.noreply.github.com>
2022-03-13 20:50:47 -0600

    Java doc

--- src/main/java/com/github/yeetmanlord/reflection_api/NMSObjectReflection.java
+++ src/main/java/com/github/yeetmanlord/reflection_api/NMSObjectReflection.java
@@ -1,6 +1,7 @@
 package com.github.yeetmanlord.reflection_api;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -8,15 +9,20 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class NMSObjectReflection {
+
 	protected Object nmsObject;
 
 	protected NMSObjectReflection(Object nmsObject) {
+
 		this.nmsObject = nmsObject;
+
 	}
 
 	/**
 	 * Will create a reflection based getting the nms equivalent of a Bukkit object
-	 * Despite bukkitObject being an object it still must be an actual Bukkit object with the method being the method to get the nms equivalent
+	 * Despite bukkitObject being an object it still must be an actual Bukkit object
+	 * with the method being the method to get the nms equivalent
+	 * 
 	 * @param bukkitObject A Bukkit object that you can get an NMS equivalent with a
 	 *                     method
 	 * @param methodName   The name of the method to get an NMS equivalent of a
@@ -24,46 +30,71 @@ public class NMSObjectReflection {
 	 * @throws NoSuchMethodException throws when given method doesn't exist
 	 */
 	public NMSObjectReflection(Object bukkitObject, String methodName) {
+
 		try {
 			this.nmsObject = bukkitObject.getClass().getMethod(methodName).invoke(bukkitObject);
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | SecurityException e) {
+		}
+		catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | SecurityException e) {
 			e.printStackTrace();
-		} catch (NoSuchMethodException e) {
+		}
+		catch (NoSuchMethodException e) {
 			System.err.println(methodName + " is not a valid method to get an nms object!");
 		}
+
 	}
 
 	/**
 	 * This creates a reflections using a classes constructor
 	 */
 	public NMSObjectReflection(String className, @Nullable Class<?>[] classes, Object[] args) {
+
 		Constructor<?> constr;
+
 		if (classes == null) {
+
 			try {
 				constr = ReflectionApi.getNMSClass(className).getConstructor();
 				this.nmsObject = constr.newInstance();
-			} catch (Exception e) {
+			}
+			catch (Exception e) {
 				e.printStackTrace();
 			}
-		} else {
+
+		}
+		else {
+
 			if (args != null) {
+
 				try {
 					constr = ReflectionApi.getNMSClass(className).getConstructor(classes);
 					this.nmsObject = constr.newInstance(args);
-				} catch (Exception e) {
+				}
+				catch (Exception e) {
 					e.printStackTrace();
 				}
+
 			}
+
 		}
+
 	}
 
+	/**
+	 * @param fieldName The name of a field
+	 * @return Returns the <i>value</i> of a field not the field itself. To get a
+	 *         {@link Field} object use {@link #getField(String)}
+	 */
 	public Object getFieldFromNmsObject(String fieldName) {
+
 		try {
 			return nmsObject.getClass().getField(fieldName).get(this.nmsObject);
-		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+		}
+		catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
 			e.printStackTrace();
 		}
+
 		return null;
+
 	}
 
 	/**
@@ -77,20 +108,26 @@ public class NMSObjectReflection {
 	 *                               name does not exist
 	 * @return The output of the method or null if it is a void method
 	 */
-	public Object invokeMethodForNmsObject(String methodName, @Nonnull Class<?>[] argsClasses, @Nonnull Object[] args)
-			throws NoSuchMethodException {
+	public Object invokeMethodForNmsObject(String methodName, @Nonnull Class<?>[] argsClasses, @Nonnull Object[] args) throws NoSuchMethodException {
+
 		Method method;
+
 		try {
 			method = nmsObject.getClass().getMethod(methodName, argsClasses);
+
 			if (method.getReturnType().equals(void.class)) {
 				method.invoke(this.nmsObject, args);
 				return null;
 			}
+
 			return method.invoke(this.nmsObject, args);
-		} catch (SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+		}
+		catch (SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 			e.printStackTrace();
 		}
+
 		return null;
+
 	}
 
 	/**
@@ -102,17 +139,44 @@ public class NMSObjectReflection {
 	 *                               name does not exist
 	 */
 	public Object invokeMethodForNmsObject(String methodName) throws NoSuchMethodException {
+
 		Method method;
+
 		try {
 			method = nmsObject.getClass().getMethod(methodName);
+
 			if (method.getReturnType().equals(void.class)) {
 				method.invoke(this.nmsObject);
 				return null;
 			}
+
 			return method.invoke(this.nmsObject);
-		} catch (SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+		}
+		catch (SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 			e.printStackTrace();
 		}
+
 		return null;
+
 	}
+
+	/**
+	 * 
+	 * @param fieldName The field you want to get
+	 * @return Returns a {@link Field} object of the {@link #nmsObject}. To get the <i>value</i> of the field use {@link #getFieldFromNmsObject(String)}
+	 * @throws NoSuchFieldException This is thrown when the field doesn't exist
+	 */
+	public Field getField(String fieldName) throws NoSuchFieldException {
+
+		try {
+			return nmsObject.getClass().getField(fieldName);
+		}
+		catch (IllegalArgumentException | NoSuchFieldException | SecurityException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+
+	}
+
 }
--- src/main/java/com/github/yeetmanlord/reflection_api/Version.java
+++ src/main/java/com/github/yeetmanlord/reflection_api/Version.java
@@ -3,83 +3,99 @@ package com.github.yeetmanlord.reflection_api;
 public class Version implements Comparable<Version> {
 	private String version;
 	private int majorVersion, minorVersion;
-	
-	public Version(String version) 
-	{
+
+	public Version(String version) {
 		this.version = version;
 		String[] split = version.replace(".", "_").split("_");
 		this.majorVersion = Integer.valueOf(split[1]);
 		this.minorVersion = 0;
-		if(split.length == 3)
-		{
+		if (split.length == 3) {
 			this.minorVersion = Integer.valueOf(split[2]);
 		}
 	}
-	
-	public Version(int major, int minor)
-	{
-		if(major < 0)
-		{
+
+	public Version(int major, int minor) {
+		if (major < 0) {
 			major = 0;
 		}
 		this.version = "1." + major;
-		if(minor > 0)
-		{
+		if (minor > 0) {
 			this.version = version + "." + minor;
 		}
 		this.majorVersion = major;
 		this.minorVersion = minor;
 	}
-	
+
 	/**
 	 * @param object Can be a {@link String} or {@link Version}
 	 */
 	@Override
 	public boolean equals(Object object) {
-		if(object instanceof Version)
-		{
-			return ((Version)object).getVersion().equals(this.version);
-		}
-		else if(object instanceof String)
-		{
+		if (object instanceof Version) {
+			return ((Version) object).getVersion().equals(this.version);
+		} else if (object instanceof String) {
 			return object == this.version;
 		}
 		return false;
 	}
-	
+
 	@Override
 	public String toString() {
 		return version;
+	}
+
+	/**
+	 * @param version String of version
+	 * @return Returns true if the current server version is older than the inputed
+	 *         version. This does not return true if the version is the same.
+	 */
+	public boolean isOlder(String version) {
+		int compare = compareTo(new Version(version));
+		return compare == -1;
+	}
+
+	/**
+	 * @param version String of version
+	 * @return Returns true if the current server version is newer than the inputed
+	 *         version.
+	 * @apiNote This will also return true if the versions are the same.
+	 * @implNote If you are checking for version 1.16 for things like netherite. The
+	 *           way to check for 1.16 would be {@link Version#isNewer("1.16")}.
+	 */
+	public boolean isNewer(String version) {
+		int compare = compareTo(new Version(version));
+		return compare == 1 || compare == 0;
 	}
 
 	@Override
 	public int compareTo(Version version) {
 		return compare(this.majorVersion, version.majorVersion, this.minorVersion, version.minorVersion);
 	}
-	
-	public int compare(int major1, int major2, int minor1, int minor2)
-	{
-		if(major1 < major2 || (major1 == major2 && minor1 < minor2))
-		{
+
+	/**
+	 * @return A return value of -1 means the inputed version is older than the
+	 *         current server version. A return value of 0 means the versions are
+	 *         the same. A return value of 1 means the current server version is
+	 *         older than the inputed version
+	 */
+	public int compare(int major1, int major2, int minor1, int minor2) {
+		if (major1 < major2 || (major1 == major2 && minor1 < minor2)) {
 			return -1;
-		}
-		else if(major1 == major2 && minor1 == minor2)
-		{
+		} else if (major1 == major2 && minor1 == minor2) {
 			return 0;
 		}
 		return 1;
 	}
-	
+
 	public String getVersion() {
 		return version;
 	}
-	
+
 	public int getMajorVersion() {
 		return majorVersion;
 	}
-	
+
 	public int getMinorVersion() {
 		return minorVersion;
 	}
-
 }
