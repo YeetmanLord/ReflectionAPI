77be70265a829522798de230c9ebb6d49e90621b
YeetmanLord <60905636+YeetmanLord@users.noreply.github.com>
2022-03-27 13:32:20 -0600

    changes

--- src/main/java/com/github/yeetmanlord/reflection_api/server/NMSServerReflection.java
+++ src/main/java/com/github/yeetmanlord/reflection_api/server/NMSServerReflection.java
@@ -1,17 +1,39 @@
 package com.github.yeetmanlord.reflection_api.server;
 
+import org.bukkit.Server;
+
 import com.github.yeetmanlord.reflection_api.NMSObjectReflection;
 import com.github.yeetmanlord.reflection_api.world.NMSWorldServerReflection;
 
-public class NMSServerReflection extends NMSObjectReflection{
+public class NMSServerReflection extends NMSObjectReflection {
 
-	public NMSServerReflection(NMSWorldServerReflection world) 
-	{
+	public NMSServerReflection(NMSWorldServerReflection world) {
+
 		super(world.getNMSServer());
+
+	}
+
+	public NMSServerReflection(Server server) {
+
+		super(server, "getServer");
+
+	}
+
+	public void stop() {
+
+		try {
+			this.invokeMethodForNmsObject("stop");
+		}
+		catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public Object getNmsServer() {
+
 		return nmsObject;
+
 	}
 
 }
--- src/main/java/com/github/yeetmanlord/reflection_api/world/NMSWorldServerReflection.java
+++ src/main/java/com/github/yeetmanlord/reflection_api/world/NMSWorldServerReflection.java
@@ -13,30 +13,42 @@ import com.github.yeetmanlord.reflection_api.players.NMSPlayerReflection;
 import com.github.yeetmanlord.reflection_api.server.NMSServerReflection;
 
 public class NMSWorldServerReflection extends NMSObjectReflection {
+
 	private NMSServerReflection server;
 
 	public NMSWorldServerReflection(World bukkitWorld) {
+
 		super(bukkitWorld, "getHandle");
 		this.server = new NMSServerReflection(this);
+
 	}
 
 	public Object getNmsWorldServer() {
+
 		return nmsObject;
+
 	}
 
 	public Object getNMSServer() {
+
 		Method getServer;
+
 		try {
 			getServer = nmsObject.getClass().getMethod("getMinecraftServer");
 			return getServer.invoke(nmsObject);
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
+
 		return null;
+
 	}
 
 	public NMSServerReflection getServer() {
+
 		return server;
+
 	}
 
 	/**
@@ -45,38 +57,52 @@ public class NMSWorldServerReflection extends NMSObjectReflection {
 	 * @return The entity list of a server but as reflected entities
 	 */
 	public List<NMSEntityReflection> entityList() {
+
 		List<NMSEntityReflection> reflectedEntityList = new ArrayList<>();
 		List<Object> entityList;
+
 		try {
 			entityList = (List<Object>) nmsObject.getClass().getField("entityList").get(nmsObject);
+
 			for (int x = 0; x < entityList.size(); x++) {
 				NMSEntityReflection entity = new NMSEntityReflection(entityList.get(x));
+
 				if (NMSPlayerReflection.isInstance(entity)) {
 					reflectedEntityList.add(new NMSPlayerReflection(entityList.get(x)));
-				} else
-					reflectedEntityList.add(entity);
+				}
+				else reflectedEntityList.add(entity);
+
 			}
-		} catch (Exception e) {
+
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
+
 		return reflectedEntityList;
+
 	}
 
 	public void addEntity(NMSEntityReflection entity) {
+
 		try {
-			nmsObject.getClass().getMethod("addEntity", ReflectionApi.getNMSClass("Entity")).invoke(nmsObject,
-					entity.getNmsEntity());
-		} catch (Exception e) {
+			nmsObject.getClass().getMethod("addEntity", ReflectionApi.getNMSClass("Entity")).invoke(nmsObject, entity.getNmsEntity());
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 
 	public void removeEntity(NMSEntityReflection entity) {
+
 		try {
-			nmsObject.getClass().getMethod("removeEntity", ReflectionApi.getNMSClass("Entity")).invoke(nmsObject,
-					entity.getNmsEntity());
-		} catch (Exception e) {
+			nmsObject.getClass().getMethod("removeEntity", ReflectionApi.getNMSClass("Entity")).invoke(nmsObject, entity.getNmsEntity());
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
+
 }
