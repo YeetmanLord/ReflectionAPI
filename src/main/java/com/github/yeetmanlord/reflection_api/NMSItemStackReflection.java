5c7a788aa49829c12dd3ef6af39419d20ced8ff1
YeetmanLord <60905636+YeetmanLord@users.noreply.github.com>
2022-09-30 20:21:09 -0600

    ItemStack NBT

--- src/main/java/com/github/yeetmanlord/reflection_api/NMSItemStackReflection.java
+++ src/main/java/com/github/yeetmanlord/reflection_api/NMSItemStackReflection.java
@@ -3,71 +3,102 @@ package com.github.yeetmanlord.reflection_api;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import com.github.yeetmanlord.reflection_api.nbt.NMSNBTTagCompoundReflection;
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_8_R3.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 
 public class NMSItemStackReflection extends NMSObjectReflection {
 
-	public NMSItemStackReflection(Object nmsObject) {
+    public NMSItemStackReflection(Object nmsObject) {
 
-		super(nmsObject);
+        super(nmsObject);
 
-	}
+    }
 
-	public NMSItemStackReflection(ItemStack stack) {
+    public NMSItemStackReflection(ItemStack stack) {
 
-		super(init(stack));
+        super(init(stack));
 
-	}
+    }
 
-	public NMSItemStackReflection(Material mat, int amount) {
+    public NMSItemStackReflection(Material mat, int amount) {
 
-		this(new ItemStack(mat, amount));
+        this(new ItemStack(mat, amount));
 
-	}
+    }
 
-	public NMSItemStackReflection(Material mat, int amount, short damage) {
+    public NMSItemStackReflection(Material mat, int amount, short damage) {
 
-		this(new ItemStack(mat, amount, damage));
+        this(new ItemStack(mat, amount, damage));
 
-	}
+    }
 
-	public NMSItemStackReflection(Material mat) {
+    public NMSItemStackReflection(Material mat) {
 
-		this(new ItemStack(mat));
+        this(new ItemStack(mat));
 
-	}
+    }
 
-	public NMSItemStackReflection(Material mat, int amount, short damage, Byte data) {
+    public NMSItemStackReflection(Material mat, int amount, short damage, Byte data) {
 
-		this(new ItemStack(mat, amount, damage, data));
+        this(new ItemStack(mat, amount, damage, data));
 
-	}
+    }
 
-	private static Object init(ItemStack stack) {
+    private static Object init(ItemStack stack) {
 
-		try {
-			Method asNMSCopy = ReflectionApi.getCraftBukkitClass("CraftItemStack", "inventory").getMethod("asNMSCopy", ItemStack.class);
-			return asNMSCopy.invoke(null, stack);
-		}
-		catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			e.printStackTrace();
-		}
+        try {
+            Method asNMSCopy = ReflectionApi.getCraftBukkitClass("CraftItemStack", "inventory").getMethod("asNMSCopy", ItemStack.class);
+            return asNMSCopy.invoke(null, stack);
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException |
+                 InvocationTargetException e) {
+            e.printStackTrace();
+        }
 
-		return null;
+        return null;
 
-	}
+    }
 
-	public static final Class<?> staticClass = ReflectionApi.getNMSClass("ItemStack");
+    public static final Class<?> staticClass = ReflectionApi.getNMSClass("ItemStack");
 
-	public static NMSItemStackReflection cast(NMSObjectReflection refl) {
+    public static NMSItemStackReflection cast(NMSObjectReflection refl) {
 
-		if (staticClass.isInstance(refl.getNmsObject())) {
-			return new NMSItemStackReflection(refl.getNmsObject());
-		}
+        if (staticClass.isInstance(refl.getNmsObject())) {
+            return new NMSItemStackReflection(refl.getNmsObject());
+        }
 
-		throw new ClassCastException("Cannot cast " + refl.toString() + " to NMSItemStackReflection");
+        throw new ClassCastException("Cannot cast " + refl.toString() + " to NMSItemStackReflection");
 
-	}
+    }
+
+    public NMSNBTTagCompoundReflection getTag() {
+        try {
+            return new NMSNBTTagCompoundReflection(invokeMethodForNmsObject("getTag"));
+        } catch (NoSuchMethodException exc) {
+            exc.printStackTrace();
+        }
+        return null;
+    }
+
+    public ItemStack asBukkit() {
+        Class<?> craftItemStack = ReflectionApi.getCraftBukkitClass("CraftItemStack", "inventory");
+        try {
+            Method asCraftMirror = craftItemStack.getMethod("asCraftMirror", staticClass);
+            return (ItemStack) asCraftMirror.invoke(null, this.getNmsObject());
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException |
+                 InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void setTag(NMSNBTTagCompoundReflection tag) {
+        try {
+            this.invokeMethodForNmsObject("setTag", new Class<?>[]{NMSNBTTagCompoundReflection.staticClass}, new Object[]{tag.getNmsObject()});
+        } catch (NoSuchMethodException exc) {
+            exc.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/com/github/yeetmanlord/reflection_api/nbt/NMSNBTTagCompoundReflection.java
+++ src/main/java/com/github/yeetmanlord/reflection_api/nbt/NMSNBTTagCompoundReflection.java
@@ -7,154 +7,260 @@ import com.github.yeetmanlord.reflection_api.NMSObjectReflection;
 import com.github.yeetmanlord.reflection_api.ReflectionApi;
 
 public class NMSNBTTagCompoundReflection extends NMSObjectReflection {
-	
-	public NMSNBTTagCompoundReflection(Object nmsObject) {
 
-		super(nmsObject);
+    public NMSNBTTagCompoundReflection(Object nmsObject) {
 
-	}
+        super(nmsObject);
 
-	public NMSNBTTagCompoundReflection() {
+    }
 
-		super(init());
+    public NMSNBTTagCompoundReflection() {
 
-	}
+        super(init());
 
-	private static Object init() {
+    }
 
-		Constructor<?> constr;
+    private static Object init() {
 
-		try {
-			constr = ReflectionApi.getNMSClass("NBTTagCompound").getConstructor();
-			return constr.newInstance();
-		}
-		catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-			e.printStackTrace();
-			return null;
-		}
+        Constructor<?> constr;
 
-	}
+        try {
+            constr = ReflectionApi.getNMSClass("NBTTagCompound").getConstructor();
+            return constr.newInstance();
+        } catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException |
+                 IllegalArgumentException | InvocationTargetException e) {
+            e.printStackTrace();
+            return null;
+        }
 
-	public void setByte(String key, byte value) {
+    }
 
-		try {
-			this.invokeMethodForNmsObject("setByte", new Class<?>[] { String.class, byte.class }, new Object[] { key, value });
-		}
-		catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		}
+    public void setByte(String key, byte value) {
 
-	}
+        try {
+            this.invokeMethodForNmsObject("setByte", new Class<?>[]{String.class, byte.class}, new Object[]{key, value});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
-	public void setShort(String key, short value) {
+    }
 
-		try {
-			this.invokeMethodForNmsObject("setShort", new Class<?>[] { String.class, short.class }, new Object[] { key, value });
-		}
-		catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		}
+    public void setCompound(String key, NMSNBTTagCompoundReflection tag) {
+        try {
+            this.invokeMethodForNmsObject("set", new Class<?>[]{String.class, ReflectionApi.getNMSClass("NBTBase")}, new Object[]{key, tag.getNmsObject()});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
 
-	}
+    public void setShort(String key, short value) {
 
-	public void setInt(String key, int value) {
+        try {
+            this.invokeMethodForNmsObject("setShort", new Class<?>[]{String.class, short.class}, new Object[]{key, value});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
-		try {
-			this.invokeMethodForNmsObject("setInt", new Class<?>[] { String.class, int.class }, new Object[] { key, value });
-		}
-		catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		}
+    }
 
-	}
+    public void setInt(String key, int value) {
 
-	public void setLong(String key, long value) {
+        try {
+            this.invokeMethodForNmsObject("setInt", new Class<?>[]{String.class, int.class}, new Object[]{key, value});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
-		try {
-			this.invokeMethodForNmsObject("setLong", new Class<?>[] { String.class, long.class }, new Object[] { key, value });
-		}
-		catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		}
+    }
 
-	}
+    public void setLong(String key, long value) {
 
-	public void setFloat(String key, float value) {
+        try {
+            this.invokeMethodForNmsObject("setLong", new Class<?>[]{String.class, long.class}, new Object[]{key, value});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
-		try {
-			this.invokeMethodForNmsObject("setFloat", new Class<?>[] { String.class, float.class }, new Object[] { key, value });
-		}
-		catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		}
+    }
 
-	}
+    public void setFloat(String key, float value) {
 
-	public void setDouble(String key, double value) {
+        try {
+            this.invokeMethodForNmsObject("setFloat", new Class<?>[]{String.class, float.class}, new Object[]{key, value});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
-		try {
-			this.invokeMethodForNmsObject("setDouble", new Class<?>[] { String.class, double.class }, new Object[] { key, value });
-		}
-		catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		}
+    }
 
-	}
+    public void setDouble(String key, double value) {
 
-	public void setString(String key, String value) {
+        try {
+            this.invokeMethodForNmsObject("setDouble", new Class<?>[]{String.class, double.class}, new Object[]{key, value});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
-		try {
-			this.invokeMethodForNmsObject("setString", new Class<?>[] { String.class, String.class }, new Object[] { key, value });
-		}
-		catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		}
+    }
 
-	}
+    public void setString(String key, String value) {
 
-	public void setByteArray(String key, byte[] value) {
+        try {
+            this.invokeMethodForNmsObject("setString", new Class<?>[]{String.class, String.class}, new Object[]{key, value});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
-		try {
-			this.invokeMethodForNmsObject("setByteArray", new Class<?>[] { String.class, byte[].class }, new Object[] { key, value });
-		}
-		catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		}
+    }
 
-	}
+    public void setByteArray(String key, byte[] value) {
 
-	public void setIntArray(String key, int[] value) {
+        try {
+            this.invokeMethodForNmsObject("setByteArray", new Class<?>[]{String.class, byte[].class}, new Object[]{key, value});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
-		try {
-			this.invokeMethodForNmsObject("setIntArray", new Class<?>[] { String.class, int[].class }, new Object[] { key, value });
-		}
-		catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		}
+    }
 
-	}
+    public void setIntArray(String key, int[] value) {
 
-	public void setBoolean(String key, boolean value) {
+        try {
+            this.invokeMethodForNmsObject("setIntArray", new Class<?>[]{String.class, int[].class}, new Object[]{key, value});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
-		try {
-			this.invokeMethodForNmsObject("setBoolean", new Class<?>[] { String.class, boolean.class }, new Object[] { key, value });
-		}
-		catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		}
+    }
 
-	}
+    public void setBoolean(String key, boolean value) {
 
-	public static final Class<?> staticClass = ReflectionApi.getNMSClass("NBTTagCompound");
+        try {
+            this.invokeMethodForNmsObject("setBoolean", new Class<?>[]{String.class, boolean.class}, new Object[]{key, value});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
 
-	public static NMSNBTTagCompoundReflection cast(NMSObjectReflection refl) {
+    }
 
-		if (staticClass.isInstance(refl.getNmsObject())) {
-			return new NMSNBTTagCompoundReflection(refl.getNmsObject());
-		}
+    public byte getByte(String key) {
+        try {
+            return (byte) this.invokeMethodForNmsObject("getByte", new Class<?>[]{String.class}, new Object[]{key});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
 
-		throw new ClassCastException("Cannot cast " + refl.toString() + " to NMSNBTTagCompoundReflection");
+    public short getShort(String key) {
+        try {
+            return (short) this.invokeMethodForNmsObject("getShort", new Class<?>[]{String.class}, new Object[]{key});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
 
-	}
+    public int getInt(String key) {
+        try {
+            return (int) this.invokeMethodForNmsObject("getInt", new Class<?>[]{String.class}, new Object[]{key});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public long getLong(String key) {
+        try {
+            return (long) this.invokeMethodForNmsObject("getLong", new Class<?>[]{String.class}, new Object[]{key});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public float getFloat(String key) {
+        try {
+            return (float) this.invokeMethodForNmsObject("getFloat", new Class<?>[]{String.class}, new Object[]{key});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public double getDouble(String key) {
+        try {
+            return (double) this.invokeMethodForNmsObject("getDouble", new Class<?>[]{String.class}, new Object[]{key});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public String getString(String key) {
+        try {
+            return (String) this.invokeMethodForNmsObject("getString", new Class<?>[]{String.class}, new Object[]{key});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public byte[] getByteArray(String key) {
+        try {
+            return (byte[]) this.invokeMethodForNmsObject("getByteArray", new Class<?>[]{String.class}, new Object[]{key});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public int[] getIntArray(String key) {
+        try {
+            return (int[]) this.invokeMethodForNmsObject("getIntArray", new Class<?>[]{String.class}, new Object[]{key});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean getBoolean(String key) {
+        try {
+            return (boolean) this.invokeMethodForNmsObject("getBoolean", new Class<?>[]{String.class}, new Object[]{key});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean hasKey(String key) {
+        try {
+            return (boolean) this.invokeMethodForNmsObject("hasKey", new Class<?>[]{String.class}, new Object[]{key});
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public NMSNBTTagCompoundReflection getCompound(String key) {
+        try {
+            return new NMSNBTTagCompoundReflection(this.invokeMethodForNmsObject("getCompound", new Class<?>[]{String.class}, new Object[]{key}));
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static final Class<?> staticClass = ReflectionApi.getNMSClass("NBTTagCompound");
+
+    public static NMSNBTTagCompoundReflection cast(NMSObjectReflection refl) {
+
+        if (staticClass.isInstance(refl.getNmsObject())) {
+            return new NMSNBTTagCompoundReflection(refl.getNmsObject());
+        }
+
+        throw new ClassCastException("Cannot cast " + refl.toString() + " to NMSNBTTagCompoundReflection");
+
+    }
 
 }
